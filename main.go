576aa1120cbc94d35ef530fdfece98fa81576640
Md Omar Faroque <faroque.ae@gmail.com>
2019-04-17 13:19:36 -0500

    adding mongodb functionalities

--- main.go
+++ main.go
@@ -2,14 +2,36 @@ package main
 
 import (
 	_ "./controllers"
+	"context"
 	"fmt"
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	_ "go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	_ "go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"golang-rest-api-best-practice/controllers"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+
+	if err!= nil{
+		return
+	}
+	fmt.Print(client)
+	fmt.Println("Connected to MongoDB!")
+
+	collection := client.Database("testing").Collection("numbers")
+	res, err := collection.InsertOne(ctx, bson.M{"name": "pi", "value": 3.14159})
+	id := res.InsertedID
+	fmt.Println(id)
+
 	router := mux.NewRouter()
 	router.HandleFunc("/people", controllers.GetPeople).Methods("GET")
 	fmt.Print("Server has started")
@@ -18,4 +40,3 @@ func main() {
 }
 
 // Display all from the people var
-
